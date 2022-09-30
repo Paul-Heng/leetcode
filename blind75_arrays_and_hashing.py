2e0283a832d929dde9809e17033cd044a07c66a1
Paul-Heng <paulheng91@gmail.com>
2022-09-29 21:48:27 -0700

    Testing

---
+++ blind75_arrays_and_hashing.py
@@ -0,0 +1,15 @@
+# Arrays & Hashing 
+# 217. Contains Duplicate (Easy)
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashSet = set()
+        
+        for num in nums:
+            if num in hashSet:
+                return True
+            else:
+                hashSet.add(num)
+                
+        return False
