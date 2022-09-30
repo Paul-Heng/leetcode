74193c37ae406172dee3c891ba9c0cb0668bd5b3
Paul-Heng <paulheng91@gmail.com>
2022-09-29 22:02:08 -0700

    Added comments explaining the anagram solution

--- blind75_arrays_and_hashing.py
+++ blind75_arrays_and_hashing.py
@@ -1,10 +1,11 @@
-# Arrays & Hashing 
+# Arrays & Hashing #1
 # 217. Contains Duplicate (Easy)
 # https://leetcode.com/problems/contains-duplicate/
 
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
         hashSet = set()
         
         for num in nums:
@@ -16,19 +17,29 @@ class Solution:
         return False
 
 
-# Arrays & Hashing 
+# Arrays & Hashing #2
 # 242. Valid Anagram (Easy)
 # https://leetcode.com/problems/valid-anagram/
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        # Checks if the lens equal each other. If they don't we know it is false. 
         if len(s) != len(t):
             return False
 
-        countS, countT = {}, {}
+        # Initilize the hashmaps
+        countStrings = {}
+        countStringt = {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-            
-        return countS == countT
+            # Parameter:	    Description
+            # keyname:	        Required. The keyname of the item you want to return the value from
+            # value:	        Optional. A value to return if the specified key does not exist.
+            #                   Default value None
+
+            countStrings[s[i]] = countStrings.get(s[i], 0) + 1
+            countStringt[t[i]] = countStringt.get(t[i], 0) + 1
+
+        # Returns true if the hashmaps equal each other. Returns false if they do not.
+        return countStrings == countStringt
